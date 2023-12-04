729d481c2cc6f075eb1a1e6052ce0cabb12d5457
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-04 02:53:26 -0800

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ptyhon3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    y = my_list[0]
+    for x in my_list:
+        if x > y:
+            y = x
+    return (y)
