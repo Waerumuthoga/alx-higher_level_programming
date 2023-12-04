08d89afa3e6ce524a863120d79385c994fd1f751
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-04 02:24:10 -0800

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    a = len(matrix)
+    for x in range(a):
+        for y in range(len(matrix[x])):
+            print('{:d}'.format(matrix[x][y]), end='')
+            if y is not (len(matrix[x]) - 1):
+                print(end=" ")
+        print("")
