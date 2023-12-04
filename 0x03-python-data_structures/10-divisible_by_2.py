12edd82ff6faf283f100e584886eec835f87bc62
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-04 03:09:05 -0800

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new = []
     for w in my_list:
-        if w % 2 is 0:
+        if w % 2 == 0:
             new.append(True)
         else:
             new.append(False)
