db48228cdfa4f38a7b05037ccd20391e1906809b
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-18 11:02:25 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    last = 0
+    try:
+        for a in range(x):
+            print(my_list[a], end="")
+            last += 1
+    except Exception as b:
+        print()
+        return(last)
+    print()
+    return(last)
