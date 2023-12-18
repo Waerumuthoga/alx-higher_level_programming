650e0a93b16f6a846fb0781cf681da6823d5c72d
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-18 11:32:53 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    last = 0
     try:
+        last = 0
         for a in range(x):
             print(my_list[a], end="")
             last += 1
