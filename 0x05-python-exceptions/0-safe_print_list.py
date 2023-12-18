1fb4a929effd7e1cfb16ab65b18789d4aa02e9a0
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-18 11:16:56 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for a in range(x):
             print(my_list[a], end="")
             last += 1
-    except:
+    except Exception:
         print()
         return(last)
     print()
