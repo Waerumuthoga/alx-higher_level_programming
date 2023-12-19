133122c0ff9ab3777d7e7d324fbc99c9233b65a4
Waerumuthoga <waerumuthoga@gmail.com>
2023-12-19 12:51:43 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Class Square that defines a square object."""
+
+    def __init__(self, size):
+        """Initializes methods(size) which stores the size of the square
+        Args:
+            parameter1 (int): size of square
+        """
+        self.__size = size
