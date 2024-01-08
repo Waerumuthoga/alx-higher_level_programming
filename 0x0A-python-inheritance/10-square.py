15e75fc3e2838ff1fd29f6bcb017ecb01e7a72ba
Waerumuthoga <waerumuthoga@gmail.com>
2024-01-08 12:56:29 -0800

    mandatory tasks

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a rectangle subclass Square"""
-Rectangle =__import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
