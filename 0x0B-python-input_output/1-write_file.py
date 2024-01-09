fa6d0d091bc9252f67189841c7f42aa22cf02714
Waerumuthoga <waerumuthoga@gmail.com>
2024-01-09 11:55:49 -0800

    mandatory tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename"", text=""):
     """ Function that writes to a text file.
     """
 
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
