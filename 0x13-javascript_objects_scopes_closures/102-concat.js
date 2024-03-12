618c771a4506b7cb82320cff898bb79482207480
Waerumuthoga <waerumuthoga@gmail.com>
2024-03-12 15:29:52 -0700

    extra tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-const list = require('./101-data.js').list;
-const newList = {};
-for (const key in list) {
-  if (newList[list[key]] === undefined) {
-    newList[list[key]] = [];
-  }
-  newList[list[key]].push(key);
-}
+const fs = require('fs');
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
