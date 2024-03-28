1fb79c49a08092b4935516787e400e374f41eb9a
Waerumuthoga <waerumuthoga@gmail.com>
2024-03-28 16:55:13 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 import urllib.request as req
 
 if __name__ == "__main__":
-    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        data = response.read()
+    with req.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        page = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
