53ebfebd762e62c566a114e3471354e4009536f7
Waerumuthoga <waerumuthoga@gmail.com>
2024-03-31 12:41:25 -0700

    advanced task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,19 +5,18 @@
             ./100-github_commits.py <repo name> <owner>
 """
 import requests
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(
-        sys.argv[2], sys.argv[1])
-    header = {"Accept": "application/vnd.github.v3+json"}
-    r = requests.get(url, headers=header)
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    r = requests.get(url)
+    commits = r.json()
     try:
-        json = r.json()
         for i in range(10):
-            print("{}: {}".format(
-                json[i].get('sha'),
-                json[i].get('commit').get('author').get('name')))
-    except ValueError:
-        print("Not a valid JSON")
+            print("{}: {}".format(commits[i].get("sha"),
+                  commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
