c5a4b18e4a1762b83af646484d1858535c54b9fa
Waerumuthoga <waerumuthoga@gmail.com>
2024-04-23 14:37:45 -0700

    advanced tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
+// script that prints the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const json = JSON.parse(body);
+    console.log(json.title);
+  }
+}
+);
