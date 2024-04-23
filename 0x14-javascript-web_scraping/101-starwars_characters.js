966f903352c7148091c3f3b335a013bb3505dda6
Waerumuthoga <waerumuthoga@gmail.com>
2024-04-23 14:41:45 -0700

    advanced tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,47 +1,23 @@
 #!/usr/bin/node
-const base64 = require('base-64');
+// script that prints all characters of a Star Wars movie:
+// Display one character name by line in the same order of the list “characters” in the /films/ response
 const request = require('request');
-const utf8 = require('utf8');
-
-let promise = new Promise(function (resolve, reject) {
-  const token = utf8.decode(base64.encode(`${process.argv[2]}:${process.argv[3]}`));
-  const options = {
-    url: 'https://api.twitter.com/oauth2/token',
-    headers: {
-      Authorization: `Basic ${token}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    form: {
-      grant_type: 'client_credentials'
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const json = JSON.parse(body).characters;
+    for (const character of json) {
+      request(character, function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          const json = JSON.parse(body);
+          console.log(json.name);
+        }
+      });
     }
-  };
-  request.post(options, function (error, response, body) {
-    if (!error) {
-      resolve(JSON.parse(body).access_token);
-    }
-  });
-});
-
-promise.then(
-  result => search(result),
-  error => console.log(error)
-);
-
-function search (bearer) {
-  const options = {
-    url: 'https://api.twitter.com/1.1/search/tweets.json',
-    headers: {
-      Authorization: `Bearer ${bearer}`
-    },
-    qs: {
-      q: process.argv[4],
-      count: '5'
-    }
-  };
-  request.get(options, function (error, response, body) {
-    if (!error) {
-      const tweets = JSON.parse(body).statuses;
-      tweets.forEach((t) => console.log(`[${t.id}] ${t.text} by ${t.user.name}`));
-    }
-  });
+  }
 }
+);
